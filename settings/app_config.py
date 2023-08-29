bc0de9418a2648df6cddc1a5b93a5d2a7dffec69
alexdoria <sr.alexdoria@gmail.com>
2023-08-29 17:49:04 +0000

    Added logic to handle missing config file

--- settings/app_config.py
+++ settings/app_config.py
@@ -4,6 +4,17 @@ import json
 
 def get_config():
     """Fetch config file"""
-    with open("./settings/config.json", encoding="utf-8") as cfg:
-        config = json.load(cfg)
+    try:
+        with open("./settings/config.json", encoding="utf-8") as cfg:
+            config = json.load(cfg)
+    except FileNotFoundError:
+        print("Configuration file was not found, please type your API credentials")
+        key = input("Type your API KEY: ")
+        host = input("Enter host url: ")
+        config = {
+            "rapidapi": {
+                "key": key,
+                "host": host
+            }
+        }
     return config
